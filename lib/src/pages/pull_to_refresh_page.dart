6a03924b70352184e5208248b9447dd0b88b94e6
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2021-04-24 15:57:31 +0900

    Change dio to http in pull-to-refresh

--- lib/src/pages/pull_to_refresh_page.dart
+++ lib/src/pages/pull_to_refresh_page.dart
@@ -1,17 +1,19 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:http/http.dart' as http;
 
 import '../localizer/localizer.dart';
 
-final _githubApiProvider =
-    Provider((_) => Dio(BaseOptions(baseUrl: 'https://api.github.com/')));
-
-final _githubUserProvider = FutureProvider<Map<String, Object?>>((ref) async {
-  final api = ref.watch(_githubApiProvider);
-  final response = await api.get<Map<String, Object?>>('users/Riscait');
-  print(response);
-  return response.data!;
+final _githubUserProvider =
+    FutureProvider.family<Map<String, Object?>, String>((ref, username) async {
+  final response = await http.get(Uri.https(
+    'api.github.com',
+    'users/$username',
+  ));
+  print(response.body);
+  return json.decode(response.body);
 });
 
 class PullToRefreshPage extends StatelessWidget {
@@ -29,14 +31,16 @@ class PullToRefreshPage extends StatelessWidget {
 class _Content extends ConsumerWidget {
   const _Content({Key? key}) : super(key: key);
 
+  final String username = 'Riscait';
+
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    return watch(_githubUserProvider).when(
+    return watch(_githubUserProvider(username)).when(
       loading: () => const Center(child: CircularProgressIndicator()),
       error: (error, stack) => Center(child: Text('Error: $error, $stack')),
       data: (user) {
         return RefreshIndicator(
-          onRefresh: () => context.refresh(_githubUserProvider),
+          onRefresh: () => context.refresh(_githubUserProvider(username)),
           child: ListView(
             padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
             children: [
