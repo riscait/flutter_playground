536741d19fa02cf9071a2f3072e6a65b6bfd9c78
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2021-07-26 21:04:39 +0900

    Add default FLAVOR

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:collection/collection.dart';
 import 'src/app.dart';
 import 'src/top_level_providers/flavor.dart';
 import 'src/top_level_providers/shared_preferences.dart';
@@ -9,9 +9,10 @@ import 'src/top_level_providers/shared_preferences.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // ビルド時に指定したFlavorを取得
+  // ビルド時に指定したFlavorを取得（未指定の場合は development とする）
   const flavorString = String.fromEnvironment('FLAVOR');
-  final flavor = Flavor.values.firstWhere((e) => e.key == flavorString);
+  final flavor = Flavor.values.firstWhereOrNull((e) => e.key == flavorString) ??
+      Flavor.development;
 
   runApp(
     ProviderScope(
